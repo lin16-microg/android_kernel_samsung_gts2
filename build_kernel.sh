d085780089602014ddea4e8bfc98806bead6cd81
MSe <mse1969@posteo.de>
2019-02-20 17:38:57 +0100

    Adapt build_kernel.sh for test compiles of 'gts2' kernel
     Modify the Samsung-delivered build_kernel.sh file to perform
     test compilations within relative position in build tree
    
     Cleanup after happens via 'make mrproper'
    
    Change-Id: I1f986f0650158ae2bb48f85cd9eaf031f987417b

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+#
+# Via the command 'make mrproper', all the messy stuff will be purged
+# after having compiled a kernel within this directiory
+#
+
+# Calculate the toolchain directory based on relative position in build tree
+pushd ../../..
+CROOT=$PWD
+popd
+
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$CROOT/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
 make exynos5433-gts210lte_defconfig
 make
